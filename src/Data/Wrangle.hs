8d7e90343c4e761ea80c190b3898b65533aaa75e
Nick Griffiths <ngriffiths21@gmail.com>
2020-06-09 16:54:12 -0400

    selection

--- src/Data/Wrangle.hs
+++ src/Data/Wrangle.hs
@@ -1,15 +1,27 @@
-module Data.Wrangle ( runPipe
+module Data.Wrangle ( DataFrame
+                    , runPipe
                     , logState
+                    , select
+                    , names
                     ) where
 
 import Control.Monad.State.Lazy
+import qualified Data.Text as T
 
-type Data = [String]
+type DataFrame = [(T.Text, [T.Text])]
 
 logState :: (Show s) => StateT s IO ()
 logState = do
   st <- get
   liftIO $ putStrLn $ show st
 
-runPipe :: StateT Data IO a -> IO a
+runPipe :: StateT DataFrame IO a -> IO a
 runPipe pipe = evalStateT pipe []
+
+select :: [T.Text] -> StateT DataFrame IO ()
+select vars = do
+  df <- get
+  put $ filter (flip elem vars . fst) df
+
+names :: StateT DataFrame IO [T.Text]
+names = get >>= return . fmap fst
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,6 +1,6 @@
 import Test.Hspec
 import Data.Wrangle as W
--- import Control.Monad.State.Lazy
+import Control.Monad.State.Lazy
 
 main :: IO ()
 main = hspec $ do
@@ -11,3 +11,14 @@ main = hspec $ do
   describe "Data.Wrangle.log" $ do
     it "can log the current state" $ do
       W.runPipe W.logState
+
+  describe "Data.Wrangle.select" $ do
+    it "can select some variables" $ do
+      res `shouldReturn` ["a"] where
+        res = W.runPipe $ do
+          put testData
+          select ["a"]
+          names
+
+testData :: W.DataFrame
+testData = [("a", ["a value 1"]), ("b", ["b value 1"])]
