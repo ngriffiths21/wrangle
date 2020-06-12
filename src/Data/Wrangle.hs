a3bf9edfb8d46703520f5cd6d88ba6644b2e984f
Nick Griffiths <ngriffiths21@gmail.com>
2020-06-12 19:33:43 -0400

    add mutations

--- src/Data/Wrangle.hs
+++ src/Data/Wrangle.hs
@@ -1,35 +1,66 @@
-module Data.Wrangle ( DataFrame
+module Data.Wrangle ( Column
+                    , Mutation (Mutation)
                     , runPipe
                     , logState
                     , select
                     , filt
                     , names
                     , dims
+                    , execute
                     ) where
 
 import Control.Monad.State.Lazy
 import qualified Data.Text as T
 import Data.Bifunctor
 
-type DataFrame = [(T.Text, [T.Text])]
+type ColName = T.Text
+type Record = [T.Text]
+
+data Mutation = Mutation { cols :: [ColName]
+                         , mut :: [Record] -> Record
+                         }
+
+instance Show Mutation where
+  show x = "Mutation"
+
+type Column = (ColName, Either Record Mutation)
+
+type ConcreteColumn = (ColName, Record)
 
 logState :: (Show s) => StateT s IO ()
 logState = do
   st <- get
   liftIO $ putStrLn $ show st
 
-runPipe :: StateT DataFrame IO a -> IO a
+runPipe :: StateT [Column] IO a -> IO a
 runPipe pipe = evalStateT pipe []
 
-select :: [T.Text] -> StateT DataFrame IO ()
+execute :: StateT [Column] IO [ConcreteColumn]
+execute = do
+  df <- get
+  let nms = fmap fst df
+  pure $ fmap (runMutation df) nms
+
+runMutation :: [Column] -> ColName -> ConcreteColumn
+runMutation cols name = let
+  loc = head $ selectByName [name] cols
+  in case loc of
+       (nm, Left r) -> (nm, r)
+       (nm, (Right mt)) -> (nm, doMutate cols mt)
+
+doMutate :: [Column] -> Mutation -> Record
+doMutate df Mutation{cols, mut} = let
+  concrete = fmap (runMutation df) cols 
+  in mut (fmap snd concrete)
+  
+select :: [T.Text] -> StateT [Column] IO ()
 select vars = modify $ selectByName vars
 
-filt :: T.Text -> (T.Text -> Bool) -> StateT DataFrame IO ()
-filt var f = do
-  df <- get
-  let selection = fmap f (snd . head $ selectByName [var] df)
-      out = fmap (second (filtBool selection)) df
-  put out
+filt df f var = let
+  selection = fmap f (snd . head $ selectByName [var] df)
+  out = fmap (second (filtBool selection)) df
+  in out
+
 
 filtBool :: [Bool] -> [a] -> [a]
 filtBool b xs = fmap snd $ filter fst (zip b xs)
@@ -37,10 +68,20 @@ filtBool b xs = fmap snd $ filter fst (zip b xs)
 selectByName :: Eq a => [a] -> [(a, b)] -> [(a, b)]
 selectByName vars df = filter (flip elem vars . fst) df
 
-names :: StateT DataFrame IO [T.Text]
+{-
+calc :: (T.Text -> T.Text) -> ([T.Text] -> [T.Text]) -> [T.Text] -> StateT [Column] IO ()
+calc namef valf cols = do
+  df <- get
+  let old = selectByName cols df
+      trans (nms, vals) = (namef nms, valf vals)
+      out = fmap trans old
+  put (df ++ out)
+-}
+
+names :: StateT [Column] IO [T.Text]
 names = get >>= return . fmap fst
 
-dims :: StateT DataFrame IO [Int]
+dims :: StateT [Column] IO [Int]
 dims = do
   df <- get
   pure [length (snd (head df)), length df]
--- test/Spec.hs
+++ test/Spec.hs
@@ -20,14 +20,29 @@ main = hspec $ do
                 names
         res `shouldReturn` ["a"]
 
+{-
   describe "Data.Wrangle.filter" $ do
     it "can filter out rows" $ do
       let res = runPipe $ do
             put testData
-            df <- get
             filt "a" (=="bar")
             dims
       res `shouldReturn` [1,2]
+-}
+  describe "Data.Wrangle.execute" $ do
+    it "can make a calculation" $ do
+      let res = runPipe $ do
+            put testDataMut
+            execute
+      res `shouldReturn`
+        [("a", ["abc", "def"]), ("f", ["abcfizz", "deffizz"])]
 
-testData :: DataFrame
-testData = [("a", ["foo", "bar"]), ("b", ["foobar", "foobaz"])]
+
+testData :: [Column]
+testData = [("a", Left ["foo", "bar"]), ("b", Left ["foobar", "foobaz"])]
+
+testDataGrp :: [Column]
+testDataGrp = [("group", Left ["1","1","2","2"]), ("value", Left ["a","b","c","d"])]
+
+testDataMut :: [Column]
+testDataMut = [("a", Left ["abc", "def"]), ("f", Right $ Mutation ["a"] (\[x] -> fmap (<>"fizz") x))]
